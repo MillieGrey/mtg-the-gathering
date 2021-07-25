ff08864bac4d454c4176d9d4b892471680955055
milliegrey <garethgreyscale@gmail.com>
2021-07-24 23:48:22 -0500

    not much

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-let boardState = new Object({
+const boardState = {
     playerCount:3,
     turn:1,
-})
+}
 
+const player = {
+    lifeTotal:20,
+    manaPool:"",
+    name:"",
+    
+    isActivePlayer:false
+}
 
+const spell = {
+    type:"",
+    stackPosition:0,
+}
 
-console.log(boardState)
+console.log(player)
