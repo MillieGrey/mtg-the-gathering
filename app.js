0b9b435e411fb263b7f3231771acb1ffd8ad246f
MillieGrey <garethgreyscale@gmail.com>
2021-08-02 03:22:26 -0500

    Started to set up a front end

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const boardState = {
-    playerCount:3,
-    turn:1,
-}
+// i dont really know what i'm doing yet so this might end up elsewhere.
+
+var startButton = document.getElementById("startButton");
+var gameSpace = document.getElementById("gameSpace");
+var battleField = document.getElementById("battleField");
+var playerRow1 = document.getElementById("playerRow1");
+var playerRow2 = document.getElementById("playerRow2");
+
+//game start script,  sets the state to active, probably gonna tie the "gameclock" to the priority system, will have that chake for gamestate, do pausing with that.
+gameStart = function () {
+    if ((boardState.gameState = "inactive")) {
+        gameSpace.innerHTML = "wat";
+        boardState.gameState = "active";
+    }
+    console.log(boardState.gameState);
+};
+startButton.addEventListener("click", gameStart);
+
+//objects?
+var boardState = {
+    gameState: "inactive",
+    playerCount: 3,
+    turn: 1,
+};
 
 const player = {
-    lifeTotal:20,
-    manaPool:"",
-    name:"",
-    
-    isActivePlayer:false
-}
+    lifeTotal: 20,
+    manaPool: "",
+    name: "",
+
+    isActivePlayer: false,
+};
 
 const spell = {
-    type:"",
-    stackPosition:0,
-}
+    type: "",
+    stackPosition: 0,
+};
 
-console.log(player)
+console.log(boardState.gameState);
